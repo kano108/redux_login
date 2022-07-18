9ca7f80dd9802366889aba3ef721aa0478a0032c
akshit <akshit.kanani.contractor@eqonex.com>
2022-07-18 15:02:25 +0530

    image display

--- src/component/home.js
+++ src/component/home.js
@@ -1,9 +1,26 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import "./home.css"
 
 function Home() {
+  const [image, setImage] = useState([]);
+
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/photos")
+      .then((data) => {
+        return data.json();
+      })
+      .then((RightData) => {
+        setImage(RightData);
+      });
+  }, []);
   return (
-    <div>Home Page</div>
-  )
+    <div className="image_box">
+      {image.map((elem) => {    
+            return <img className="per_image" src={elem.url}/>
+        
+      })}
+    </div>
+  );
 }
 
-export default Home
+export default Home;
